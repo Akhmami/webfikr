6240cef01f6f73e52c5bcd8e21ab2e3d86ac616b
MD Akmami <akhmami@gmail.com>
2021-07-25 14:38:44 +0700

    update mobilephones migration

--- database/migrations/2021_06_21_044622_create_mobile_phones_table.php
+++ database/migrations/2021_06_21_044622_create_mobile_phones_table.php
@@ -15,6 +15,7 @@ class CreateMobilePhonesTable extends Migration
     {
         Schema::create('mobile_phones', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->string('country_code');
             $table->string('number', 15);
             $table->enum('is_first', ['Y', 'N'])->default('N');
