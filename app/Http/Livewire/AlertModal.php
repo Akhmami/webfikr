3ead79d16c66d3e6698478ea76d89808e9803a7e
MD Akmami <akhmami@gmail.com>
2021-07-27 23:44:22 +0700

    remove modal width

--- app/Http/Livewire/AlertModal.php
+++ app/Http/Livewire/AlertModal.php
@@ -34,20 +34,20 @@ class AlertModal extends ModalComponent
         }
     }
 
-    public static function modalMaxWidth(): string
-    {
-        // 'sm'
-        // 'md'
-        // 'lg'
-        // 'xl'
-        // '2xl'
-        // '3xl'
-        // '4xl'
-        // '5xl'
-        // '6xl'
-        // '7xl'
-        return 'sm';
-    }
+    // public static function modalMaxWidth(): string
+    // {
+    //     // 'sm'
+    //     // 'md'
+    //     // 'lg'
+    //     // 'xl'
+    //     // '2xl'
+    //     // '3xl'
+    //     // '4xl'
+    //     // '5xl'
+    //     // '6xl'
+    //     // '7xl'
+    //     return 'sm';
+    // }
 
     public static function closeModalOnClickAway(): bool
     {
--- app/Http/Livewire/User/AlertModal.php
+++ app/Http/Livewire/User/AlertModal.php
@@ -13,20 +13,20 @@ class AlertModal extends ModalComponent
         return view('livewire.user.alert-modal');
     }
 
-    public static function modalMaxWidth(): string
-    {
-        // 'sm'
-        // 'md'
-        // 'lg'
-        // 'xl'
-        // '2xl'
-        // '3xl'
-        // '4xl'
-        // '5xl'
-        // '6xl'
-        // '7xl'
-        return 'sm';
-    }
+    // public static function modalMaxWidth(): string
+    // {
+    //     // 'sm'
+    //     // 'md'
+    //     // 'lg'
+    //     // 'xl'
+    //     // '2xl'
+    //     // '3xl'
+    //     // '4xl'
+    //     // '5xl'
+    //     // '6xl'
+    //     // '7xl'
+    //     return 'sm';
+    // }
 
     public static function closeModalOnClickAway(): bool
     {
