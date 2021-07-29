8b769db919258961ab3030ad6531dde917dbf073
MD Akmami <akhmami@gmail.com>
2021-07-29 11:02:58 +0700

    change VA library

--- app/Http/Livewire/User/RincianTagihan.php
+++ app/Http/Livewire/User/RincianTagihan.php
@@ -47,8 +47,6 @@ class RincianTagihan extends ModalComponent
                     'datetime_expired' => date('c', strtotime('2 days'))
                 );
 
-                dd($data);
-
                 $va = new VA;
                 $result = $va->create($data);
 
--- app/Libraries/VA.php
+++ app/Libraries/VA.php
@@ -26,6 +26,8 @@ class VA
         //     $data_asli['virtual_account'] = str_replace(substr($data['virtual_account'], 0, 8), '98810789', $data['virtual_account']);
         // }
 
+        dd($data_asli);
+
         $res = $this->hashing($data_asli);
 
         if ($res['status'] !== '000') {
