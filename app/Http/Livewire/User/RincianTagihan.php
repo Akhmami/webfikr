324cdff208526478bdc495eb9206c2027bb59657
MD Akmami <akhmami@gmail.com>
2021-09-05 00:35:27 +0700

    decline zero amount

--- app/Http/Livewire/User/RincianTagihan.php
+++ app/Http/Livewire/User/RincianTagihan.php
@@ -31,39 +31,43 @@ class RincianTagihan extends ModalComponent
         $cek_billing = Billing::where('virtual_account', $no_pendaftaran)
             ->active()->exists();
 
-        if (!$cek_billing) {
-            if ($biller->is_installment === 'Y') {
-                $this->emit('openModal', 'user.cicilan', ['biller' => $biller]);
-            } else {
-                $jenjang = $biller->user->userDetail->jenjang;
-                $trx_id = $biller->type . $jenjang . date('YmdHis');
-
-                $data = array(
-                    'trx_id' => $trx_id,
-                    'user_id' => $biller->user->id,
-                    'virtual_account' => $no_pendaftaran,
-                    'trx_amount' => $biller->amount,
-                    'billing_type' => 'c',
-                    'customer_name' => auth()->user()->name,
-                    'description' => 'Pembayaran ' . $biller->type,
-                    'datetime_expired' => date('c', strtotime('2 days'))
-                );
-
-                $va = new VA;
-                $result = $va->create($data);
-
-                if ($result['status'] !== '000') {
-                    $this->emit('openModal', 'user.alert-modal', ['message' => 'Gagal memproses tagihan, silahkan coba lagi jika masih berlanjut hubungi kami. #' . $result['status']]);
+        if ($biller->amount > 0) {
+            if (!$cek_billing) {
+                if ($biller->is_installment === 'Y') {
+                    $this->emit('openModal', 'user.cicilan', ['biller' => $biller]);
                 } else {
-                    $data['datetime_expired'] = date('Y-m-d H:i:s', strtotime('2 days'));
-                    $biller->billings()->create($data);
-                    return redirect()->to(route('user.pembayaran'));
-                }
-            }
+                    $jenjang = $biller->user->userDetail->jenjang;
+                    $trx_id = $biller->type . $jenjang . date('YmdHis');
 
-            UserActivity::dispatch(auth()->user(), 'Memproses tagihan, tombol Bayar Sekarang diklik');
+                    $data = array(
+                        'trx_id' => $trx_id,
+                        'user_id' => $biller->user->id,
+                        'virtual_account' => $no_pendaftaran,
+                        'trx_amount' => $biller->amount,
+                        'billing_type' => 'c',
+                        'customer_name' => auth()->user()->name,
+                        'description' => 'Pembayaran ' . $biller->type,
+                        'datetime_expired' => date('c', strtotime('2 days'))
+                    );
+
+                    $va = new VA;
+                    $result = $va->create($data);
+
+                    if ($result['status'] !== '000') {
+                        $this->emit('openModal', 'user.alert-modal', ['message' => 'Gagal memproses tagihan, silahkan coba lagi jika masih berlanjut hubungi kami. #' . $result['status']]);
+                    } else {
+                        $data['datetime_expired'] = date('Y-m-d H:i:s', strtotime('2 days'));
+                        $biller->billings()->create($data);
+                        return redirect()->to(route('user.pembayaran'));
+                    }
+                }
+
+                UserActivity::dispatch(auth()->user(), 'Memproses tagihan, tombol Bayar Sekarang diklik');
+            } else {
+                $this->emit('openModal', 'user.alert-modal', ['message' => 'Masih ada pembayaran yang belum diselesaikan, selengkapnya klik <a href="' . route('user.pembayaran') . '" class="underline text-indigo-600">disini</a>']);
+            }
         } else {
-            $this->emit('openModal', 'user.alert-modal', ['message' => 'Masih ada pembayaran yang belum diselesaikan, selengkapnya klik <a href="' . route('user.pembayaran') . '" class="underline text-indigo-600">disini</a>']);
+            $this->emit('openModal', 'user.alert-modal', ['message' => 'Pembayaran tidak dapat di proses, tagihan kosong']);
         }
     }
 }
