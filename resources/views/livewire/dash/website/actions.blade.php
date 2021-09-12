5aa8ab17432b5b499f57b3c638f31cc875f30244
MD Akmami <akhmami@gmail.com>
2021-09-13 06:10:25 +0700

    dashboard

--- resources/views/livewire/dash/website/actions.blade.php
+++ resources/views/livewire/dash/website/actions.blade.php
@@ -29,10 +29,10 @@
             class="z-10 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
             role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class="py-1" role="none">
-                <a href="#"
+                {{-- <a href="#"
                     onclick="Livewire.emit('openModal', 'dash.keuangan.billing-detail', {{ json_encode(['billing' => $data->id]) }})"
-                    class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1"
-                    id="menu-item-1">Detail</a>
+                class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1"
+                id="menu-item-1">Detail</a> --}}
             </div>
         </div>
     </div>
