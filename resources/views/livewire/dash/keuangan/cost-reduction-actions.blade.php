7b97a5a9d1c896315e63ee482d2cf4fb15cfd230
MD Akmami <akhmami@gmail.com>
2021-09-17 09:39:14 +0700

    add action keringanan

--- resources/views/livewire/dash/keuangan/cost-reduction-actions.blade.php
+++ resources/views/livewire/dash/keuangan/cost-reduction-actions.blade.php
@@ -17,14 +17,14 @@
             class="z-10 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
             role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class="py-1" role="none">
-                <a href="#"
-                    onclick="Livewire.emit('openModal', 'dash.keuangan.cost-reduction-edit', {{ json_encode(['reduction' => $data->id]) }})"
-                    class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1"
-                    id="menu-item-1">edit</a>
                 {{-- <a href="#"
-                    onclick="Livewire.emit('openModal', 'dash.keuangan.cost-reduction-delete', {{ json_encode(['reduction' => $data->id]) }})"
+                    onclick="Livewire.emit('openModal', 'dash.keuangan.cost-reduction-edit', {{ json_encode(['reduction' => $data->id]) }})"
                 class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1"
-                id="menu-item-1">hapus</a> --}}
+                id="menu-item-1">edit</a> --}}
+                <a href="#"
+                    onclick="Livewire.emit('openModal', 'dash.keuangan.cost-reduction-delete', {{ json_encode(['reduction' => $data->id]) }})"
+                    class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1"
+                    id="menu-item-1">hapus</a>
             </div>
         </div>
     </div>
