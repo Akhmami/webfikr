190f1ee289cd60fab9c2f795e383ac43f27a152b
MD Akmami <akhmami@gmail.com>
2021-09-27 13:58:19 +0700

    add mobile phone detile

--- resources/views/livewire/dash/psb/pendaftar-detail.blade.php
+++ resources/views/livewire/dash/psb/pendaftar-detail.blade.php
@@ -59,6 +59,31 @@
                         @endif
                     </dd>
                 </div>
+                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">
+                        Nomor HP
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        <ul class="border border-gray-200 rounded-md divide-y divide-gray-200">
+                            @forelse ($user->mobilePhones as $mobile)
+                            <li class="p-3 flex items-center justify-between text-sm">
+                                <div class="w-0 flex-1 flex items-center">
+                                    <span class="flex-1 w-0 truncate">
+                                        {{ $mobile->name }}
+                                    </span>
+                                </div>
+                                <div class="ml-4 flex-shrink-0">
+                                    <span class="font-medium text-indigo-600">
+                                        {{ $mobile->full_number }}
+                                    </span>
+                                </div>
+                            </li>
+                            @empty
+                            no. HP tidak tersedia <a href="#">Tambahkan</a>
+                            @endforelse
+                        </ul>
+                    </dd>
+                </div>
             </dl>
         </div>
     </div>
