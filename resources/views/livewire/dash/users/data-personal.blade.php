1de442a3126f8f16245455488871ed569e1bbf15
MD Akmami <akhmami@gmail.com>
2021-10-01 11:31:40 +0700

    edit nik

--- resources/views/livewire/dash/users/data-personal.blade.php
+++ resources/views/livewire/dash/users/data-personal.blade.php
@@ -69,7 +69,18 @@
         </div>
         <div class="sm:col-span-1">
             <dt class="text-sm font-medium text-gray-500">NIK</dt>
-            <dd class="mt-1 text-sm text-gray-900">{{ $user->userDetail->nik }}</dd>
+            <dd class="flex space-x-2 mt-1 text-sm text-gray-900">
+                <div>{{ $user->userDetail->nik }}</div>
+                <a href="#"
+                    onclick="Livewire.emit('openModal', 'dash.edit-value', {{ json_encode(['model' => 'UserDetail', 'id' => $user->userDetail->id, 'column' => 'nik']) }})"
+                    class="p-1 rounded-md hover:bg-yellow-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500 hover:text-gray-900"
+                        viewBox="0 0 20 20" fill="currentColor">
+                        <path
+                            d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                    </svg>
+                </a>
+            </dd>
         </div>
         <div class="sm:col-span-1">
             <dt class="text-sm font-medium text-gray-500">NIS // NISN</dt>
