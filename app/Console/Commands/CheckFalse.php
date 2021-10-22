30c14ad4091680037dfdfdf0daf0d4fa5697491f
MD Akmami <akhmami@gmail.com>
2021-10-22 08:43:15 +0700

    salah daftar

--- app/Console/Commands/CheckFalse.php
+++ app/Console/Commands/CheckFalse.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Libraries\SheetDB;
 use App\Models\User;
+use App\Models\UserDetail;
 use Illuminate\Console\Command;
 
 class CheckFalse extends Command
@@ -38,18 +40,21 @@ class CheckFalse extends Command
      */
     public function handle()
     {
-        $users = User::has('activeGrade')->with('billerSPP')->cursor();
-
-        foreach ($users as $user) {
-            $spp = $user->billerSPP;
-            if (!empty($spp)) {
-                $qty = $spp->qty_spp;
-                $count = $spp->billerDetails->count();
-
-                if ($qty != $count) {
-                    $this->error($user->name);
-                }
+        foreach ($this->generator() as $item) {
+            $userDetail = UserDetail::where('no_pendaftaran', $item->no_pendaftaran)->first();
+            if ($userDetail) {
+                $userDetail->jenis_pendaftaran = 'internal';
+                $userDetail->save();
             }
+        }
+    }
+
+    private function generator()
+    {
+        $sheets = SheetDB::get('https://sheetdb.io/api/v1/n8q4yb5v6rps1');
+
+        foreach ($sheets as $sheet) {
+            yield $sheet;
         }
     }
 }
