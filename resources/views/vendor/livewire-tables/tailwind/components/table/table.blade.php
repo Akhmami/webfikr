baa6b7c06478c81606959f9776a48e3e33a1d579
MD Akmami <akhmami@gmail.com>
2021-12-14 06:34:25 +0700

    callback history

--- resources/views/vendor/livewire-tables/tailwind/components/table/table.blade.php
+++ resources/views/vendor/livewire-tables/tailwind/components/table/table.blade.php
@@ -1,13 +1,17 @@
-<div class="align-middle max-w-full overflow-hidden shadow rounded-none md:rounded-lg">
-    <table {{ $attributes->except('wire:sortable') }} class="w-auto divide-y divide-gray-200">
-        <thead>
-            <tr>
-                {{ $head }}
-            </tr>
-        </thead>
+<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="align-middle overflow-hidden shadow rounded-none md:rounded-lg">
+            <table {{ $attributes->except('wire:sortable') }} class="min-w-full divide-y divide-gray-200">
+                <thead>
+                    <tr>
+                        {{ $head }}
+                    </tr>
+                </thead>
 
-        <tbody {{ $attributes->only('wire:sortable') }} class="bg-white divide-y divide-gray-200">
-            {{ $body }}
-        </tbody>
-    </table>
+                <tbody {{ $attributes->only('wire:sortable') }} class="bg-white divide-y divide-gray-200">
+                    {{ $body }}
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
