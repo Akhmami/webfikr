f80525afa9384339933b8cffa3efc5e3c22b1ada
MD Akmami <akhmami@gmail.com>
2021-12-14 06:36:27 +0700

    callback history

--- resources/views/vendor/livewire-tables/tailwind/components/table/table.blade.php
+++ resources/views/vendor/livewire-tables/tailwind/components/table/table.blade.php
@@ -1,4 +1,4 @@
-<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+<div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div class="align-middle overflow-hidden shadow rounded-none md:rounded-lg">
             <table {{ $attributes->except('wire:sortable') }} class="min-w-full divide-y divide-gray-200">
