4af1bbbbb8329f20962e2cc87ef3b6f4cfbd8256
MD Akmami <akhmami@gmail.com>
2021-12-14 06:18:20 +0700

    callback history

--- resources/views/vendor/livewire-tables/tailwind/components/table/cell.blade.php
+++ resources/views/vendor/livewire-tables/tailwind/components/table/cell.blade.php
@@ -1,6 +1,7 @@
 @props(['customAttributes' => []])
 
-<td {{ $attributes->merge(array_merge(['class' => 'px-3 py-2 md:px-6 md:py-4 text-sm leading-5 text-gray-900'],
+<td {{ $attributes->merge(array_merge(['class' => 'px-3 py-2 md:px-6 md:py-4 break-words text-sm leading-5
+    text-gray-900'],
     $customAttributes)) }}>
     {{ $slot }}
 </td>
