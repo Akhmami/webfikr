e895a820a835574588e033d23146f623a79c08d8
MD Akmami <akhmami@gmail.com>
2021-12-20 09:53:31 +0700

    questionnaire badge on admin

--- resources/views/livewire/dash/psb/status-psb-badge.blade.php
+++ resources/views/livewire/dash/psb/status-psb-badge.blade.php
@@ -18,7 +18,7 @@ $text = [
 ];
 @endphp
 
-<div class="flex items-center">
+<div class="flex flex-col items-start space-y-1">
     <span
         class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-{{$color[$data->status_psb_id]}}-100 text-{{$color[$data->status_psb_id]}}-800">
         {{ $text[$data->status_psb_id] }}
@@ -26,11 +26,14 @@ $text = [
 
     @if ($data->questionnaire_psb === 1)
     <a href="#"
+        class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800"
         onclick="Livewire.emit('openModal', 'dash.questionnaire-answered', {{ json_encode(['user' => $data]) }})"
         title="Questionnaire terisi">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+        questionnaire
+        <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-4 w-4 text-green-500" viewBox="0 0 20 20"
+            fill="currentColor">
             <path fill-rule="evenodd"
-                d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                 clip-rule="evenodd" />
         </svg>
     </a>
