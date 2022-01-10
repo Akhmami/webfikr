5ab211604fe55b58b22c4a0c87b2ef0ab3ec3d51
MD Akmami <akhmami@gmail.com>
2022-01-10 09:57:25 +0700

    add view files

--- resources/views/livewire/dash/psb/status-psb-badge.blade.php
+++ resources/views/livewire/dash/psb/status-psb-badge.blade.php
@@ -20,7 +20,7 @@ $text = [
 
 <div class="flex flex-col items-start space-y-1">
     <div class="flex items-center space-x-1">
-        <a href="#"
+        <button
             onclick="Livewire.emit('openModal', 'dash.edit-value', {{ json_encode(['model' => 'User', 'id' => $data->id, 'column' => 'status_psb_id', 'type' => 'status-psb']) }})"
             class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-{{$color[$data->status_psb_id]}}-100 text-{{$color[$data->status_psb_id]}}-800">
             {{ $text[$data->status_psb_id] }}
@@ -29,7 +29,7 @@ $text = [
                 <path
                     d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
             </svg>
-        </a>
+        </button>
         @if ($data->status_psb_id === 3 && $data->userDetail->jenjang === 'SMA')
         <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
             {{ $data->userDetail->jurusan }}
@@ -38,8 +38,7 @@ $text = [
     </div>
 
     @if ($data->questionnaire_psb === 1)
-    <a href="#"
-        class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800"
+    <button class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800"
         onclick="Livewire.emit('openModal', 'dash.questionnaire-answered', {{ json_encode(['user' => $data]) }})"
         title="Questionnaire terisi">
         questionnaire
@@ -49,14 +48,13 @@ $text = [
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                 clip-rule="evenodd" />
         </svg>
-    </a>
+    </button>
     @endif
 
     @can('lihat berkas')
     <div class="flex items-center space-x-1">
         @forelse ($data->files as $file)
-        <a href="#"
-            class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800"
+        <button class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800"
             onclick="Livewire.emit('openModal', 'dash.questionnaire-answered', {{ json_encode(['user' => $data]) }})"
             title="Questionnaire terisi">
             {{ $file->nama }}
@@ -66,7 +64,7 @@ $text = [
                     d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
                     clip-rule="evenodd" />
             </svg>
-        </a>
+        </button>
         @empty
 
         @endforelse
