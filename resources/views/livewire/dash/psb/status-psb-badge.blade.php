84847db4c6c945efb3fe976cc560594493d7857e
MD Akmami <akhmami@gmail.com>
2022-01-10 10:12:51 +0700

    add view files

--- resources/views/livewire/dash/psb/status-psb-badge.blade.php
+++ resources/views/livewire/dash/psb/status-psb-badge.blade.php
@@ -20,8 +20,9 @@ $text = [
 
 <div class="flex flex-col items-start space-y-1">
     <div class="flex items-center space-x-1">
-        <button
+        <button @can('edit status psb')
             onclick="Livewire.emit('openModal', 'dash.edit-value', {{ json_encode(['model' => 'User', 'id' => $data->id, 'column' => 'status_psb_id', 'type' => 'status-psb']) }})"
+            @endcan
             class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-{{$color[$data->status_psb_id]}}-100 text-{{$color[$data->status_psb_id]}}-800">
             {{ $text[$data->status_psb_id] }}
             <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-4 w-4 text-gray-500 hover:text-gray-900"
@@ -54,9 +55,8 @@ $text = [
     @can('lihat berkas')
     <div class="flex items-center space-x-1">
         @forelse ($data->files as $file)
-        <button class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800"
-            onclick="Livewire.emit('openModal', 'dash.questionnaire-answered', {{ json_encode(['user' => $data]) }})"
-            title="Questionnaire terisi">
+        <a href="{{ asset('storage/file_from_user/' . $file->file) }}" target="_blank"
+            class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">
             {{ $file->nama }}
             <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-4 w-4 text-red-500 hover:text-red-900"
                 viewBox="0 0 20 20" fill="currentColor">
@@ -64,7 +64,7 @@ $text = [
                     d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
                     clip-rule="evenodd" />
             </svg>
-        </button>
+        </a>
         @empty
 
         @endforelse
