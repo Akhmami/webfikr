e6e7299238ac12a0441c041dfb49b8463a56b7db
MD Akmami <akhmami@gmail.com>
2022-05-01 15:41:03 +0700

    add eujian

--- app/Console/Commands/AddToEujian.php
+++ app/Console/Commands/AddToEujian.php
@@ -67,43 +67,38 @@ class AddToEujian extends Command
             }
 
             if ($allow === 1) {
-                $this->userEujian($item);
+                $curl = curl_init();
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://www.e-ujian.com/api/peserta/add',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => array(
+                        'no_peserta' => $item->no_peserta,
+                        'nama_peserta' => $item->nama_peserta,
+                        'email' => '',
+                        'kode_akses' => $item->kode_akses,
+                        'kelompok' => $item->kelompok,
+                        'tags' => 'US2022'
+                    ),
+                    CURLOPT_HTTPHEADER => array(
+                        'Authorization: Basic aHVtYXNuZmJzc2VyYW5nQGdtYWlsLmNvbTp1MnhkVmg='
+                    ),
+                ));
+
+                $response = curl_exec($curl);
+
+                curl_close($curl);
+                $this->info($response);
             }
 
             $item->hak_akses = $allow;
             $item->save();
         }
-    }
-
-    public function userEujian($item)
-    {
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://www.e-ujian.com/api/peserta/add',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => array(
-                'no_peserta' => $item->no_peserta,
-                'nama_peserta' => $item->nama_peserta,
-                'email' => '',
-                'kode_akses' => $item->kode_akses,
-                'kelompok' => $item->kelompok,
-                'tags' => 'US2022'
-            ),
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Basic aHVtYXNuZmJzc2VyYW5nQGdtYWlsLmNvbTp1MnhkVmg='
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        return $response;
     }
 }
