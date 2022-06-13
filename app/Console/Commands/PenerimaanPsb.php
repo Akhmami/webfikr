1906cdbc327608935c59be761b25b5ba6337e771
MD Akmami <akhmami@gmail.com>
2022-06-13 14:38:54 +0700

    penerimaan

--- app/Console/Commands/PenerimaanPsb.php
+++ app/Console/Commands/PenerimaanPsb.php
@@ -57,7 +57,7 @@ class PenerimaanPsb extends Command
                     $user->update([
                         'status_psb_id' => $statusPsb[$item->status_psb],
                         'status' => ($statusPsb[$item->status_psb] === 3 ? 'santri' : null),
-                        'gelombang_id' => 4
+                        'gelombang_id' => 6
                     ]);
 
                     $user->userDetail()->update([
