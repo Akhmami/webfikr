f58762df7aa522be750dc27d06853fe9c84c66cf
MD Akmami <akhmami@gmail.com>
2022-11-14 07:44:49 +0700

    update redirect after login

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\CallbackController;
-use App\Http\Controllers\Api\SurveyController;
-use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,20 +15,4 @@ use App\Http\Controllers\Api\UserController;
 
 Route::get('/{slug}', function () {
     return response()->json('Not found', 404);
-});
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/callback/payments', [CallbackController::class, 'index']);
-// Route::post('/dev/callback/payments', [CallbackController::class, 'development']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/me', [AuthController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::middleware('role:super-admin|admin')->group(function () {
-        Route::post('/user', [UserController::class, 'user']);
-        Route::post('/user/psb', [UserController::class, 'psb']);
-        Route::post('/survey/psb', [SurveyController::class, 'psb']);
-        Route::get('/user/administrasi', [UserController::class, 'administrasi']);
-    });
 });
